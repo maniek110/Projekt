595682357836bdf8c56b1ffc026fb6b5a2b2303f
IrionXenix <irion29@gmail.com>
2017-05-24 21:42:12 +0200

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("tonopraca.pl","23960364_0000002","PkHyVv7*njetPP@56","23960364_0000002");
-
-
-if (mysqli_connect_errno())
-  {
-  echo "Nie połączono z MySQL: " . mysqli_connect_error();
-  }
-?>
